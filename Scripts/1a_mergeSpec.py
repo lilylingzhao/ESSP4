b1e8415cd1af222979a9b6b01cb3dd401199c047
Lily Ling Zhao <lilylingzhao@uchicago.edu>
2025-07-28 21:15:15 -0500

    Allow merge to run on all data sets in one go

--- Scripts/1a_mergeSpec.py
+++ Scripts/1a_mergeSpec.py
@@ -12,44 +12,44 @@ def main():
     
     parser = argparse.ArgumentParser(description='Generate an ESSP IV Data Set')
     
-    parser.add_argument('-d','--data-set',type=int,
-                        help='Number of the data set to use in saved files.')
+    parser.add_argument('-d','--data-set',nargs='*',default=[],
+                        help='Numbers of data sets to run CCFs for')
+    parser.add_argument('--overwrite', action='store_true',
+                        help="Overwrite existing files")
 
     # We could consider adding arguments that define the new wavelength grid
     #     but I don't see much utility in that at present
     
     args = parser.parse_args()
+    
+    # Gather Files
+    if not args.data_set:
+        file_list = glob(os.path.join(essp_dir,'*','*','Spectra','*.fits'))
+    else:
+        file_list = []
+        for dset_num in args.data_set:
+            file_list.append(glob(os.path.join(essp_dir,'*',f'DS{dset_num}','Spectra','*.fits')))
+        file_list = np.concatenate(file_list)
+    file_list = np.sort(file_list)
+    
+    for file in tqdm(file_list,desc=f'Merging'):
+        merge_file = file.replace('Spectra','Merged').replace('_spec_','_mrge_')
 
-    # Define Data Set Name
-    data_set_num = int(args.data_set)
-    data_set_name = f'DS{data_set_num}'
+        # Read in spectrum
+        hdus = fits.open(file)
+        head = hdus[0].header
+        wave = hdus['wavelength'].data.copy()
+        spec = (hdus['flux'].data/hdus['continuum'].data).copy()
+        errs = hdus['uncertainty'].data.copy()
+        hdus.close()
+        
+        # Interpolate onto default wavelength grid
+        bind_output = bind_resample(wave,spec,errs)
 
-    for sub_folder in ['Training','Validation']:
-        dset_dir = os.path.join(essp_dir,sub_folder,data_set_name)
-
-        merge_dir = os.path.join(dset_dir,'Merged')
-        if not os.path.isdir(merge_dir):
-            os.makedirs(merge_dir)
-
-        file_list = glob(os.path.join(dset_dir,'Spectra','*.fits'))
-        for file in tqdm(file_list,desc=f'Merging {data_set_name} {sub_folder} Data'):
-            merge_file = os.path.join(merge_dir,os.path.basename(file))
-
-            # Read in spectrum
-            hdus = fits.open(file)
-            head = hdus[0].header
-            wave = hdus['wavelength'].data.copy()
-            spec = (hdus['flux'].data/hdus['continuum'].data).copy()
-            errs = hdus['uncertainty'].data.copy()
-            hdus.close()
-            
-            # Interpolate onto default wavelength grid
-            bind_output = bind_resample(wave,spec,errs)
-
-            # Save merged spectrum
-            hdu_s2d = fits.PrimaryHDU(data=None,header=head)
-            hdu_list_s2d = [hdu_s2d]
-            for ikey,key in enumerate(['wavelength','flux','uncertainty']):
-                hdu_list_s2d.append(fits.ImageHDU(data=bind_output[ikey],name=key))
-            fits.HDUList(hdu_list_s2d).writeto(merge_file,overwrite=True)
-            tqdm.write(merge_file)
+        # Save merged spectrum
+        hdu_s2d = fits.PrimaryHDU(data=None,header=head)
+        hdu_list_s2d = [hdu_s2d]
+        for ikey,key in enumerate(['wavelength','flux','uncertainty']):
+            hdu_list_s2d.append(fits.ImageHDU(data=bind_output[ikey],name=key))
+        fits.HDUList(hdu_list_s2d).writeto(merge_file,overwrite=True)
+        tqdm.write(merge_file)
