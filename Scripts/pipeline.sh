2517e2f49f80a56c7801d6ae9b73573b5ad22974
Lily Ling Zhao <lilylingzhao@uchicago.edu>
2025-07-31 06:31:03 -0500

    Data sets in sequence

--- Scripts/pipeline.sh
+++ Scripts/pipeline.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #python ./0_mkTelluricMask.py --overwrite
-conda activate expres_pipeline
 for dset in {0..9}; do
+    conda activate expres_pipeline
     planet_file="/Volumes/Hasbrouck/ceph/ESSP_Solar/4_DataSets/SuperSecretPlanets/dataset_seq_$dset.csv"
     python ./1_mkDataSet.py -d $dset --num-obs 5 --num-day 60 -v Nperday --validation-amount 2 -planet-file $planet_file
+    conda deactivate
+    python ./1a_mergeSpec.py -d $dset --overwrite
+    python ./2_runCCFs.py -d $dset --overwrite --iccf
+    python ./3_fitIndicators.py -d $dset --overwrite
 done
-conda deactivate
-
-# Code that doesn't need expres_pipeline or a specific data set
-python ./1a_mergeSpec.py --overwrite
-python ./2_runCCFs.py --overwrite --iccf
-python ./3_fitIndicators.py --overwrite
